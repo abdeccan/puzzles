65b989d38f617db7d5db9f2dc0a9622a35a44011
bkrishnamurthy <balaji_krishnamurthy@intuit.com>
2019-08-20 16:07:13 +0530

    brute force approach

--- triangletype/triangletype.cpp
+++ triangletype/triangletype.cpp
@@ -8,11 +8,31 @@ using namespace std;
 
 int main ()
 {
+	// Brute force approach
 	cout << "Enter the sides of triangle:" << endl;
 	int side1 = 0, side2 = 0, side3 = 0;
 	cin >> side1;
 	cin >> side2;
 	cin >> side3;
 	cout << "Sides are " << side1 << side2 << side3 << endl;
+	if (side1 == side2 && side2 == side3)	// equilateral
+	{
+		cout << "Equilateral triangle!" << endl;
+	}
+	else if (side1 == side2 || side2 == side3 || side3 == side1) // isoceles
+	{
+		cout << "Isoceles triangle" << endl;
+	}
+	else
+	{
+		cout << "Scalene triangle" << endl;
+	}
+
+	if (side1*side1 == side2 *side2 + side3 *side3 ||
+		side2*side2 == side3 *side3 + side1 *side1 ||
+		side3*side3 == side2 *side2 + side1 *side1)
+	{
+		cout << "Right triangle" << endl;
+	}
 	return 0;
 }
