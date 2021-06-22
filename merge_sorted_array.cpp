f8602dbc3e1af1da77c37209eee162c7ca3ae67b
bkrishnamurthy <balaji_krishnamurthy@intuit.com>
2021-06-23 00:58:12 +0530

    added better method

--- merge_sorted_array.cpp
+++ merge_sorted_array.cpp
@@ -46,45 +46,77 @@ void sort_Better(vector<int> &a1, vector<int> &a2) {
 
 	size_t s1 = a1.size();
 	size_t s2 = a2.size();
-	vector<int> a3(s1+s2);
-	
-	for(int i = 0, j = 0, k = 0; k < s1 + s2; k++) {
-		if(i > s2 - 1) { 
-			a3[k] = a1[i];
+
+	/*	
+		1	5 	9 	10 	15 	20
+
+		2 	3 	8 	13
+	*/
+	cout << endl << "before sorting" << endl;
+	for(int idx = 0; idx < s1; idx++) cout << "	" << a1[idx];
+	cout << endl;
+	for(int idx = 0; idx < s2; idx++) cout << "	" << a2[idx];
+	size_t s = std::max(s1, s2);
+	auto swap = [](int& a, int& b) {
+		int tmp = a; a = b; b = tmp;
+	};
+	for(int i = 0, j = 0, k = 0; k < s; k++) {
+		if(a1[i] > a2[j]) {
+			swap(a1[i], a2[j]);
+			//j++;
 			i++;
-			continue;
-		} 
-		if(j > s1 - 1) { 
-			a3[k] = a2[j];
-			j++;
-			continue;
-		} 
+			sort(a2.begin(), a2.end());
+		} else if(a1[i] < a2[j]) {
+			i++;
+		}
+/*		cout << endl << "---------"<< endl;
+		for(int idx = 0; idx < s1; idx++) cout << "	" << a1[idx];
+		cout << endl;
+		for(int idx = 0; idx < s2; idx++) cout << "	" << a2[idx];
+*/
 		
-		if(a1[i] < a2[j]) {
-			a3[k] = a1[i];
-			i++;
-		}
-		else {
-			a3[k] = a2[j];
-			j++;
-		}
 	}
+	cout << endl << "after sorting" << endl;
+	for(int idx = 0; idx < s1; idx++) cout << "	" << a1[idx];
 	cout << endl;
-	
-//	for(int i = 0; i < s1; i ++) cout << "	" << a1[i];
-//	for(int i = 0; i < s2; i ++) cout << "	" << a2[i];
-	for(int i = 0; i < s1 + s2; i ++) cout << "	" << a3[i];
+	for(int idx = 0; idx < s2; idx++) cout << "	" << a2[idx];
 	cout << endl;
-	// its sorted, now put it back
-	for(int i = 0; i < s1; i++) a1[i] = a3[i];
-	for(int i = s1; i < s1 + s2; i++) a2[i] = a3[i];
-	for(int i = 0; i < s1 + s2; i ++) cout << "	" << a3[i];
-	cout << endl;
+}
+inline int getElemAtGap(int idx, size_t gap, size_t s1, size_t s2) {
+	if(idx + gap > s1) {
+		return a2[idx + gap - s1];
+	}
+}
+
+void sort_Optimal(vector<int> &a1, vector<int> &a2) {
+
+	if(a1.empty() || a2.empty()) return;
+
+	/*
+		Here we are going to apply the GAP method (shell sort??) wherein we 
+		keep on taking the GAP length and reduce it in half and move the window
+		accordingly.
+	*/
+
+	size_t s = a1.size() + a2.size(); // s/2 will be first gap size
+
+	/*	
+		1	5 	9 	10 	15 	20
+
+		2 	3 	8 	13
+
+		1	5 	9 	10 	15 	20	2 	3 	8 	13
+	*/
+
+	for(int i = 0; i < s; i++) {
+		
+	}
+
 }
 
 int main () {
 	vector<int> a1 = {1,	5, 	9, 	10, 	15, 	20};
 	vector<int> a2 = {2, 	3, 	8, 	13};
-	sort_BruteForce(a1, a2);
+	sort_Better(a1, a2);
 	return 0;
 }
